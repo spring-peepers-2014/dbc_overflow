ad3e01c5661b586e52d2406b6601c842690a9106
Siyan Beverly <siyan.beverly@gmail.com>
2014-06-06 14:30:55 -0400

    Added question_controller spec skeleton

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,16 +1,47 @@
 require 'spec_helper'
 
-
 describe '#index' do
+	it 'it pulls all questions from database' do
+
+	end
 end
 
 describe '#new' do
-	it 'saves a new question to the database' do 
+	it 'navigates to the new_question link' do 
 	end
 
-	it 'rejects a question if it is below a certain length'
-	end
-
-	it '' do
+	it 'displays an error msg and does not proceed if question is below a certain length'
 	end
 end
+
+describe '#create' do
+	it 'saves the newly created question to the database' do
+	end
+end
+
+describe '#show' do
+
+	it 'finds the requested question in the database' do
+	end
+
+	it 'finds the answers associated with the requested question' do
+	end
+
+	it 'finds the comments associated with the requested question' do
+	end
+end
+
+
+
+# NOT IMPLEMENTED IN R0
+
+# describe '#edit' do
+# 	it 'finds the requested question to be edited' do
+# 	end
+# end
+
+# describe '#update' do
+# 	it 'updates the requested question with the new values' do
+# 	end
+# end
+
