643f35156aeff036d7c3e37481535ccec23b7eed
Alexander Frankel <alexxander.frankel@gmail.com>
2014-06-06 11:53:01 -0400

    dbc_overflow rails 3.2.18 template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DbcOverflow::Application.config.secret_token = '3ce78475c7309753b54d456b8f903bfcd71513cc3ba3a98307ea0716b519bd39e898c4ce29fca720562b36b9319124a3c610cab6d003948b65818986fcf5ac66'
