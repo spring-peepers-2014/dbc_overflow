ec31758a684884e4631ed1b8f9c829b887bf4901
Siyan Beverly <siyan.beverly@gmail.com>
2014-06-06 14:13:53 -0400

    setting up questions controller test

---
+++ spec/controllers/questions_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+
+describe '#index' do
+end
+
+describe '#new' do
+	it 'saves a new question to the database' do 
+	end
+
+	it 'rejects a question if it is below a certain length'
+	end
+
+	it '' do
+	end
+end
