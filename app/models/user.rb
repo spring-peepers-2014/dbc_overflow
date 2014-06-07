934bd88b73d09f4e76f245cd474347ade384be60
Ryan Rebo <ryanpatrickrebo@gmail.com>
2014-06-07 19:04:42 -0400

    Added links to pages -login page -users specific page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-  attr_accessible :name, :email, :password, :bio
+  attr_accessible :name, :email, :password_digest, :bio
 
   has_many :answers
   has_many :questions
