c77c93687a4b749f88c8645801b1f1d0c895be05
Isabel <isabel.rial@gmail.com>
2014-06-07 13:19:00 -0400

    Set questions index to root

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,16 @@
 class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+  end
+
   def new
     @question = Question.new
+  end
+
+  def create
+  end
+
+  def update
   end
 
 end
