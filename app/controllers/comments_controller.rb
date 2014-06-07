3a46f2abe6feb345209390071b172a7f7b7b5d75
Zach Karst <zach@karst.co>
2014-06-07 19:48:40 -0400

    Add comments feature to questions and answers views
    Working
    - routes
    
    Not working / to do
    - comment form
    - comment add link for answer on question page
    - modify comments controller for edited form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,32 @@
 class CommentsController < ApplicationController
 
-  def create
-    p params
-    if params[:question_id] != nil
-      Question.find(params[:question_id]).comments << Comment.create(params[:comment])
-      redirect_to :back
-    else
+  # def new
+
+  # end
+
+#   def create
+#     p params
+#     if params[:question_id] != nil
+#       Question.find(params[:question_id]).comments << Comment.create(params[:comment])
+#       redirect_to :back
+#     else
+#     end
+#   end
+# end
+
+  def new
+    p "These are the params: #{params.inspect}"
+    if !params[:answer_id].nil?
+      @commentable = Answer.find params[:answer_id]
+    elsif !params[:question_id].nil?
+      @commentable = Question.find params[:question_id]
     end
+
+    raise ActiveRecord::RecordNotFound if @commentable.nil?
+  end
+
+  def create
+    @commentable = Commentable.new :params[:comment]
+      render :new
   end
 end
