e6cdeb6e88586040435f5c60dee69285fcd65eba
Zach Karst <zach@karst.co>
2014-06-07 14:14:30 -0400

    Migrate database - change user password column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140607010247) do
+ActiveRecord::Schema.define(:version => 20140607180156) do
 
   create_table "answers", :force => true do |t|
     t.text     "body"
@@ -37,19 +37,13 @@ ActiveRecord::Schema.define(:version => 20140607010247) do
     t.datetime "updated_at", :null => false
   end
 
-  create_table "tags", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password_hash"
+    t.string   "password_digest"
     t.string   "bio"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
   create_table "votes", :force => true do |t|
