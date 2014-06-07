6c6e3908b76e4f8eb20107e66831ae52fdd977cf
Isabel <isabel.rial@gmail.com>
2014-06-07 15:05:06 -0400

    Added edit question link, fixed extraneous text in edit view

--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,12 @@ ActiveRecord::Schema.define(:version => 20140607180156) do
     t.datetime "updated_at", :null => false
   end
 
+  create_table "tags", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
