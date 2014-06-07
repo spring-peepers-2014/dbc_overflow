890ef3d580a6e0a90497e6a0800dc36e982bb3fd
Zach Karst <zach@karst.co>
2014-06-07 17:25:46 -0400

    Add create method for polymorphic  comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,23 @@
 class CommentsController < ApplicationController
 
-  #this is going connect to new.html.erb which holds the comment form
-  def new
-    @comment = Comment.new
-  end
-
-  #this handles the post request after a comment is submitted
-  #use ajax to append the comment(?)
   def create
-    @comment = Comment.create(params[:comment])
+    @comment = Comment.new(params[:comment])
+    @comment.commentable = @commentable
+
+    respond_to do |format|
+      if @comment.save
+        p "Successful"
+      else
+      end
   end
 
-
-
+  private
+  def get_object
+    type = ''
+    if params[:question_id]; type = 'question'
+    elsif params[:answer_id]; type = 'answer'
+    end
+    value = type.classify.constantize.find_by_identifier(params[:"#{type}_id"])
+    instance_variable_set("@commentable" , value)
+  end
 end
