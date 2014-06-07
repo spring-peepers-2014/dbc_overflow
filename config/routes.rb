9fa0b8f3009864496e00c11f59f5614e0aa9b64d
Zach Karst <zach@karst.co>
2014-06-07 19:26:01 -0400

    Add comments#new route for non-ajax form submission tests

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ DbcOverflow::Application.routes.draw do
   get 'logout' => 'users#logout', :as => 'user_logout'
 
   resources :questions do
-    resources :comments, only: [:create]
+    resources :comments, only: [:new, :create]
     resources :votes, only: [:create]
 
     resources :answers, only: [:new, :create, :edit, :update, :destroy] do
-      resources :comments, only: [:create]
+      resources :comments, only: [:new, :create]
       resources :votes, only: [:create]
     end
   end
