960e780e98262a61222b66310fe614c812ea8f32
Zach Karst <zach@karst.co>
2014-06-07 19:22:08 -0400

    Add comments#create working for question model - answer model not implemented

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,11 @@
 class CommentsController < ApplicationController
 
   def create
-    @comment = Comment.new(params[:comment])
-    @comment.commentable = @commentable
-
-    respond_to do |format|
-      if @comment.save
-        p "Successful"
-      else
-      end
-  end
-
-  private
-  def get_object
-    type = ''
-    if params[:question_id]; type = 'question'
-    elsif params[:answer_id]; type = 'answer'
+    p params
+    if params[:question_id] != nil
+      Question.find(params[:question_id]).comments << Comment.create(params[:comment])
+      redirect_to :back
+    else
     end
-    value = type.classify.constantize.find_by_identifier(params[:"#{type}_id"])
-    instance_variable_set("@commentable" , value)
   end
 end
