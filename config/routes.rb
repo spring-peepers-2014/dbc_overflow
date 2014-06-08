18002ea184da34ed0cfc8b09f071eb380038ba4e
Zach Karst <zach@karst.co>
2014-06-07 22:04:51 -0400

    Remove nested routes.
    Answers are no longer children of questions in url path. Comment#post is now two separate methods in the questions and answers controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@ DbcOverflow::Application.routes.draw do
   resources :users
 
   resources :questions do
-    resources :comments, only: [:new, :create]
-    resources :votes, only: [:create]
+    member do 
+      post 'comment'
+      post 'post'
+    end
+  end
 
-    resources :answers, only: [:new, :create, :edit, :update, :destroy] do
-      resources :comments, only: [:new, :create]
-      resources :votes, only: [:create]
+  resources :answers, only: [:new, :create, :edit, :update, :destroy] do
+    member do 
+      post 'comment'
+      post 'post'
     end
   end
   root to: 'questions#index'
