cd8b576ce70bc277b3e0a5be5ffb269a209bbffb
Zach Karst <zach@karst.co>
2014-06-08 15:58:35 -0400

    Add vote_value to attr_accessible

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,11 +1,12 @@
 class Question < ActiveRecord::Base
-  attr_accessible :name, :body
+  attr_accessible :name, :body, :vote_value
   validates :body, presence: true
   validates :name, presence: true
 
   has_many :answers
   has_many :tags
   has_many :comments, as: :commentable
+  has_many :votes, as: :voteable
 
   belongs_to :user
 end
