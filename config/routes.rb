c1144e4db664236aaf24034860023493a173fab8
Zach Karst <zach@karst.co>
2014-06-07 23:40:39 -0400

    Remove nested comments and votes paths. Comment#post is now #add_comment in answers and questions controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,24 @@
 DbcOverflow::Application.routes.draw do
   resources :users
+
   get 'login' => 'users#login', :as => 'user_login'
   post 'login' => 'users#verify', :as => 'user_verify'
   get 'logout' => 'users#logout', :as => 'user_logout'
 
   resources :questions do
-    member do 
+    member do
       post 'comment'
       post 'post'
+  resources :questions do
+    resources :answers, only: [:new, :create, :edit, :update, :destroy] do
+      member do
+        post 'add_comment'
+        post 'vote'
+      end
     end
-  end
-
-  resources :answers, only: [:new, :create, :edit, :update, :destroy] do
-    member do 
-      post 'comment'
-      post 'post'
+    member do
+      post 'add_comment'
+      post 'vote'
     end
   end
   root to: 'questions#index'
