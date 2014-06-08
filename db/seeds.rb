7229b958646b03991935b8d0107f30ad5351d2bd
Alexander Pan <alexanderdavidpan@gmail.com>
2014-06-08 16:01:50 -0400

    Added some seed data for creating Tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Tag.create(name: "Ruby")
+Tag.create(name: "Javascript")
+Tag.create(name: "SQL")
+Tag.create(name: "Active Record")
+Tag.create(name: "jQuery")
+Tag.create(name: "AJAX")
+Tag.create(name: "HTML")
+Tag.create(name: "CSS")
