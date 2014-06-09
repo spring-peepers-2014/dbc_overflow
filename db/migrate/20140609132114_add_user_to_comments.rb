23c1b0ab834b9b99242a33631f06b34bac0a07d6
Zach Karst <zach@karst.co>
2014-06-09 09:31:41 -0400

    Fix migration add user column to comments table

--- db/migrate/20140609132114_add_user_to_comments.rb
+++ db/migrate/20140609132114_add_user_to_comments.rb
@@ -1,8 +1,8 @@
 class AddUserToComments < ActiveRecord::Migration
   def up
-    add_column :comments, :user, :belongs_to
+    add_column :comments, :user_id, :integer
   end
   def down
-    remove_column :comments, :user, :belongs_to
+    remove_column :comments, :user_id, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140607180156) do
+ActiveRecord::Schema.define(:version => 20140609132114) do
 
   create_table "answers", :force => true do |t|
     t.text     "body"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20140607180156) do
     t.string   "commentable_type"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
+    t.integer  "user_id"
   end
 
   create_table "questions", :force => true do |t|
